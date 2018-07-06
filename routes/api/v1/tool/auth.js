f22fcf8f0159570c02c839a9c90bc36bb93d097b
shi lei <>
2018-07-06 15:04:54 +0900

    add login api

--- routes/api/v1/tool/auth.js
+++ routes/api/v1/tool/auth.js
@@ -1,8 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 
+//test
 router.get('/', function(req, res){
     res.json({"msg":"auth test"})
 })
+
+//login認証
+router.post(
+    '/login',
+    passport.authenticate('local'),
+    function(req,res, next){
+        res.json({
+            "status":200,
+            "message":"ログイン成功",
+            "userName":"テストユーザー"
+        });
+    }
+)
 
 module.exports = router;
